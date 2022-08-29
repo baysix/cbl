7a0f9bead63817e7b3b7bc0a78709d4ca5469022
yspark1 <yspark1@enliple.com>
2022-08-29 14:35:36 +0900

    router mail path 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
- 
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 //페이지
-import MainPage from './pages/main';
-import NotFound from './pages/error';
+import MainPage from "./pages/main";
+import NotFound from "./pages/error";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/cbl" element={<MainPage />} />
         <Route to="/404" element={<NotFound />} />
       </Routes>
-  </Router>
-
+    </Router>
   );
 }
 
