a6e1569c662f07804180c21a8fbc2e3f5876bd73
yspark1 <yspark1@enliple.com>
2022-09-01 14:26:08 +0900

    체리픽 테스트
    (cherry picked from commit ec6fc1e04cdbd1046ecf91c9cbd8cfdc721edfb3)

--- src/lib/api/news.js
+++ src/lib/api/news.js
@@ -3,5 +3,5 @@ import client from "../utils";
 //news 크롤링 리스트 api
 export const newsCbl = (param) => client.get(`/api/board/news/listPage`, param);
 
-//news 가지고 오고싶은 뉴스 api[c]
+//news 가지고 오고싶은 뉴스 api
 export const newsDetail = (param) => client.get(`/api/board/news/get/${param}`);
