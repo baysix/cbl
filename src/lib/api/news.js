ff57ec7fef91ac787c2cbc401d1c760349cc75cf
yspark1 <yspark1@enliple.com>
2022-09-01 18:33:30 +0900

    테스트

--- src/lib/api/news.js
+++ src/lib/api/news.js
@@ -5,3 +5,5 @@ export const newsCbl = (param) => client.get(`/api/board/news/listPage`, param);
 
 //news 가지고 오고싶은 뉴스 api
 export const newsDetail = (param) => client.get(`/api/board/news/get/${param}`);
+
+// 테스트
